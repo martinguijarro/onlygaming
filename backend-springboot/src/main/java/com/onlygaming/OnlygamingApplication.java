e96302a1b7d30c98a1e80e9a3d085184a2b0103a
martinguijarro <martin.gg222@gmail.com>
2025-05-11 13:14:00 +0200

    First commit

---
+++ backend-springboot/src/main/java/com/onlygaming/OnlygamingApplication.java
@@ -0,0 +1,13 @@
+package com.onlygaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlygamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlygamingApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/onlygaming/OnlygamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlygaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlygamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
