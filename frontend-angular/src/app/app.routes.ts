1a78cd19f60d80a60ab060c33c37e9a117427606
martinguijarro <martin.gg222@gmail.com>
2025-05-24 17:17:06 +0200

    Update proyect

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
