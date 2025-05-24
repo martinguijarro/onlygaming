d990d532430fcead211d8a1cfaa5aa4842136417
martinguijarro <martin.gg222@gmail.com>
2025-05-24 10:48:24 +0200

    Deleted RequestMapping

--- backend-springboot/src/main/java/com/onlygaming/Game/GameController.java
+++ backend-springboot/src/main/java/com/onlygaming/Game/GameController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.onlygaming.Exception.ResourceNotFoundException;
@@ -19,7 +18,6 @@ import com.onlygaming.Exception.ResourceNotFoundException;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1")
 public class GameController {
 
 	@Autowired
--- backend-springboot/src/main/java/com/onlygaming/Reply/ReplyController.java
+++ backend-springboot/src/main/java/com/onlygaming/Reply/ReplyController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.onlygaming.Exception.ResourceNotFoundException;
@@ -21,7 +20,6 @@ import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/v1")
 public class ReplyController {
     
     @Autowired
